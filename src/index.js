1ed7c95a56e50b8197ca95008810cd38a8a4d31a
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-07-26 13:33:14 +0100

    Added basic styilng to main content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import navbar from "./navbar"
+import navbar from "./navbar";
+import content from "./main";
 
 let navButton = document.querySelector(".page-header-nav-button");
 let navContent = document.querySelector(".nav-content");
-navContent.appendChild(navbar)
+let mainContent = document.querySelector(".main-content");
 navButton.addEventListener("click", () => {
 	navContent.getAttribute("aria-expanded") === "true"
-		? navContent.setAttribute("aria-expanded" , "false")
-		: navContent.setAttribute("aria-expanded" , "true")
+		? navContent.setAttribute("aria-expanded", "false")
+		: navContent.setAttribute("aria-expanded", "true");
+		navButton.classList.toggle("rotated")
 });
 
+navContent.appendChild(navbar);
+mainContent.appendChild(content);
